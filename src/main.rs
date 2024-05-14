8e8373dca93c2f86aee2f13f617f37937ec485cd
funnyboy-roks <funnyboyroks@gmail.com>
2024-05-14 11:28:33 -0500

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{
-    collections::HashMap,
-    io::{Read, Write},
-    net::TcpListener,
-};
+use std::collections::HashMap;
 
 use anyhow::Context;
+use tokio::{
+    io::{AsyncRead, AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 const CRLF: &str = "\r\n";
 
@@ -17,14 +17,16 @@ struct Request {
 }
 
 impl Request {
-    pub fn read_until<R>(r: &mut R, delim: u8) -> anyhow::Result<String>
+    pub async fn read_until<R>(r: &mut R, delim: u8) -> anyhow::Result<String>
     where
-        R: Read,
+        R: AsyncRead + Unpin,
     {
-        let mut bytes = r.bytes();
         let mut out = String::with_capacity(5);
+        let mut buf = [0u8; 1];
         loop {
-            let byte = bytes.next().context("foo")??;
+            let l = r.read_exact(&mut buf).await?;
+            assert_eq!(l, 1);
+            let byte = buf[0];
 
             if byte == delim {
                 break;
@@ -36,22 +38,22 @@ impl Request {
         Ok(out)
     }
 
-    pub fn parse<R>(r: &mut R) -> anyhow::Result<Request>
+    pub async fn parse<R>(r: &mut R) -> anyhow::Result<Request>
     where
-        R: Read,
+        R: AsyncRead + std::marker::Unpin,
     {
-        let method = Self::read_until(r, b' ')?;
-        let path = Self::read_until(r, b' ')?;
+        let method = Self::read_until(r, b' ').await?;
+        let path = Self::read_until(r, b' ').await?;
 
-        Self::read_until(r, b'\r')?;
+        Self::read_until(r, b'\r').await?;
         let mut buf = [0u8; 1];
-        r.read_exact(&mut buf)?;
+        r.read_exact(&mut buf).await?;
         assert_eq!(buf[0], b'\n');
 
         let mut headers = HashMap::new();
 
         loop {
-            let s = Self::read_until(r, b'\n')?;
+            let s = Self::read_until(r, b'\n').await?;
             let s = s.strip_suffix('\r').unwrap();
 
             if s == "" {
@@ -88,48 +90,58 @@ impl Request {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
-    // let mut buf = [0u8; 256];
-    for stream in listener.incoming() {
-        let mut stream = match stream {
-            Ok(stream) => stream,
-            Err(e) => {
-                eprintln!("Error: {:?}", e);
-                continue;
-            }
-        };
-
-        let req = Request::parse(&mut stream)?;
-        eprintln!("accepted new connection");
-        dbg!(&req);
-        match (&*req.method, &*req.path) {
-            ("GET", "/user-agent") => {
-                let ua = req.headers.get("User-Agent").unwrap();
-                write!(
-                    stream,
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    ua.len(),
-                    ua
-                )?;
-            }
-            ("GET", s) if s.starts_with("/echo/") => {
-                write!(
-                    stream,
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    s.len() - 6,
-                    s.strip_prefix("/echo/").unwrap()
-                )?;
-            }
-            ("GET", "/") => {
-                write!(stream, "HTTP/1.1 200 OK{CRLF}{CRLF}")?;
-            }
-            _ => {
-                write!(stream, "HTTP/1.1 404 Not Found{CRLF}{CRLF}")?;
-            }
+async fn handle_stream(mut stream: TcpStream) -> anyhow::Result<()> {
+    let req = Request::parse(&mut stream).await?;
+    eprintln!("accepted new connection");
+    dbg!(&req);
+    match (&*req.method, &*req.path) {
+        ("GET", "/user-agent") => {
+            let ua = req.headers.get("User-Agent").unwrap();
+            stream.write_all(
+                    format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        ua.len(),
+                        ua
+                    )
+                    .as_bytes(),
+                ).await?;
+        }
+        ("GET", s) if s.starts_with("/echo/") => {
+            stream.write_all(
+                    format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        s.len() - 6,
+                        s.strip_prefix("/echo/").unwrap()
+                    )
+                    .as_bytes(),
+                ).await?;
+        }
+        ("GET", "/") => {
+            stream.write_all(b"HTTP/1.1 200 OK{CRLF}{CRLF}").await?;
+        }
+        _ => {
+            stream
+                .write_all(b"HTTP/1.1 404 Not Found{CRLF}{CRLF}")
+                .await?;
         }
     }
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
+
+    // let mut buf = [0u8; 256];
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            match handle_stream(stream).await {
+                Ok(_) => {}
+                Err(e) => eprintln!("error: {:?}", e),
+            }
+        });
+    }
 }
